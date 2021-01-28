69a009c632936728867e0a72798ced163db471ff
ninadu07 <52179640+ninadu07@users.noreply.github.com>
2021-01-27 23:41:57 -0800

    Add files via upload
    Implemented config with Hydro/OmegaConfig

--- inc/main_hetero_last.py
+++ inc/main_hetero_last.py
@@ -15,61 +15,37 @@ from torch.utils.data import DataLoader
 from retina_dataset_simple import Retina_Dataset_Simple
 import pandas as pd
 
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    # Data
-    parser.add_argument('--data_dir', type=str, default="data")
-    parser.add_argument('--ckpt_dir', type=str, default="ckpt")
-    parser.add_argument('--name', type=str, default="baseline")
-
-    # Model
-    parser.add_argument('--model', type=str, default="resnet50")
-    parser.add_argument('--seed', type=int, default=random.randint(0, 9999999))
-
-    # Training
-    parser.add_argument('--batch_size', type=int, default=16)
-    parser.add_argument('--epoch_per', type=int, default=99)
-    parser.add_argument('--lr', type=float, default=0.0001)
-    parser.add_argument('--task_binary', type=bool, default=True)
-    parser.add_argument('--early_stop', type=int, default=10)
-
-    # Incremental
-    parser.add_argument('--num_exp', type=int, default=5)
-    parser.add_argument('--rounds', type=int, default=4)
-    parser.add_argument('--samples_start', type=int, default=10000)
-    parser.add_argument('--samples_test', type=int, default=10000)
-
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
-    args = parse_args()
+@hydra.main(config_path = 'Config')
+def hetero_last(cfg: DictConfig): 
     # Seed
-    torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
+    if cfg.seed == None: 
+        cfg.seed = random.randint(0, 9999999)
+    torch.manual_seed(cfg.seed)
+    np.random.seed(cfg.seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    ckpt_dir = os.path.join("ckpt", args.name)
+    ckpt_dir = os.path.join("ckpt", cfg.name)
     os.makedirs(ckpt_dir, exist_ok=True)
 
     test_dict = defaultdict(list)
 
     # Separate negative from positive
     samples = []
-    samples += (glob.glob(os.path.join(args.data_dir, 'train', "*")))
-    samples += (glob.glob(os.path.join(args.data_dir, 'test', "*")))
+    samples += (glob.glob(os.path.join(cfg.data_dir, 'train', "*")))
+    samples += (glob.glob(os.path.join(cfg.data_dir, 'test', "*")))
 
     csv_labels = []
-    csv_labels += csv.DictReader(open(os.path.join(args.data_dir, "trainLabels.csv")), delimiter=',')
-    csv_labels += csv.DictReader(open(os.path.join(args.data_dir, "valLabels.csv")), delimiter=',')
+    csv_labels += csv.DictReader(open(os.path.join(cfg.data_dir, "trainLabels.csv")), delimiter=',')
+    csv_labels += csv.DictReader(open(os.path.join(cfg.data_dir, "valLabels.csv")), delimiter=',')
 
     positive = set([row['image'] for row in csv_labels if int(row['level']) > 0])
     negative = set([row['image'] for row in csv_labels if int(row['level']) == 0])
 
-    for num_exp in range(args.num_exp):
+    for num_exp in range(cfg.num_exp):
 
         samples_positive = sorted([s for s in samples if os.path.basename(os.path.splitext(s)[0]) in positive],
                                   key=lambda k: random.random())
@@ -80,11 +56,11 @@ if __name__ == '__main__':
               "Negative samples:", len(samples_negative))
 
         # Initial round on balanced
-        incr = args.samples_test
+        incr = cfg.samples_test
         datas = []
 
-        end_pos = int(args.samples_start * 0.8)
-        end_neg = int(args.samples_start * 0.2)
+        end_pos = int(cfg.samples_start * 0.8)
+        end_neg = int(cfg.samples_start * 0.2)
         datas.append(sorted(
             samples_positive[0:end_pos] + samples_negative[0:end_neg],
             key=lambda k: random.random()))
@@ -99,7 +75,7 @@ if __name__ == '__main__':
 
         # Following rounds on imbalanced train and balanced test
 
-        for i in range(args.rounds):
+        for i in range(cfg.rounds):
             start_pos = end_pos
             start_neg = end_neg
             end_pos = int(start_pos + incr * 0.8)
@@ -116,30 +92,30 @@ if __name__ == '__main__':
             datas.append(sorted(samples_positive[start_pos:end_pos] + samples_negative[start_neg:end_neg],
                                 key=lambda k: random.random()))
 
-        net = eval(args.model)(pretrained=True)
-        net.fc = nn.Linear(net.fc.in_features, 2 if args.task_binary else 5)
+        net = eval(cfg.model)(pretrained=True)
+        net.fc = nn.Linear(net.fc.in_features, 2 if cfg.task_binary else 5)
         net.cuda()
         if torch.cuda.device_count() > 1:
             print("Let's use", torch.cuda.device_count(), "GPUs!")
             net = nn.DataParallel(net)
         criterion = nn.CrossEntropyLoss(reduction="sum").cuda()
-        optimizer = optim.Adam(net.parameters(), lr=args.lr)
+        optimizer = optim.Adam(net.parameters(), lr=cfg.lr)
 
-        for r in range(args.rounds):
+        for r in range(cfg.rounds):
             # Split current data round to train/val. Next round is test set
             train_round = r * 2
             test_round = train_round + 1
             eighty_percent = int(len(datas[train_round]) * 0.8)
-            train_loader = DataLoader(Retina_Dataset_Simple(datas[train_round][:eighty_percent], csv_labels, args, mode='train'),
-                                      args.batch_size, num_workers=4)
-            val_loader = DataLoader(Retina_Dataset_Simple(datas[train_round][eighty_percent:], csv_labels, args, mode='val'),
-                                    args.batch_size, num_workers=4)
-            test_loader = DataLoader(Retina_Dataset_Simple(datas[test_round], csv_labels, args, mode='test'),
-                                     args.batch_size, num_workers=4)
+            train_loader = DataLoader(Retina_Dataset_Simple(datas[train_round][:eighty_percent], csv_labels, cfg, mode='train'),
+                                      cfg.batch_size, num_workers=4)
+            val_loader = DataLoader(Retina_Dataset_Simple(datas[train_round][eighty_percent:], csv_labels, cfg, mode='val'),
+                                    cfg.batch_size, num_workers=4)
+            test_loader = DataLoader(Retina_Dataset_Simple(datas[test_round], csv_labels, cfg, mode='test'),
+                                     cfg.batch_size, num_workers=4)
             best_acc = 0
             early_stop = 0
 
-            for epoch in range(args.epoch_per):
+            for epoch in range(cfg.epoch_per):
                 net.train()
                 for iteration, data in enumerate(train_loader):
                     inputs = data['image'].cuda()
@@ -156,9 +132,9 @@ if __name__ == '__main__':
                         r + 1,
                         epoch + 1,
                         iteration,
-                        int(len(train_loader.dataset) / args.batch_size),
-                        loss.cpu().data.numpy() / args.batch_size,
-                        args.lr,  # *[group['lr'] for group in optim.param_groups],
+                        int(len(train_loader.dataset) / cfg.batch_size),
+                        loss.cpu().data.numpy() / cfg.batch_size,
+                        cfg.lr,  # *[group['lr'] for group in optim.param_groups],
                         early_stop,
                     ), end='          ')
 
@@ -180,10 +156,10 @@ if __name__ == '__main__':
                     torch.save({
                         'round': r,
                         'state_dict': net.state_dict(),
-                        'args': args,
+                        'cfg': cfg,
                     },
                         os.path.join(ckpt_dir, 'model_best.pth'))
-                if early_stop == args.early_stop:
+                if early_stop == cfg.early_stop:
                     break
 
             # Test
@@ -210,3 +186,9 @@ if __name__ == '__main__':
     mean_str += ']'
     print(mean_str)
     open(os.path.join(ckpt_dir, "test_log.txt"), 'a+').write(mean_str)
+
+
+
+if __name__ == '__main__':
+    hetero_last()
+    
